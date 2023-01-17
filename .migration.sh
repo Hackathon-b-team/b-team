15145c268ebf54110812b56ee394b22affca2f98
mappii130 <90373590+mappii130@users.noreply.github.com>
2023-01-17 14:29:22 +0900

    URLパターン追加

--- .migration.sh
+++ .migration.sh
@@ -4,4 +4,4 @@
 docker compose -f docker-compose.yml exec app python manage.py makemigrations --noinput
 docker compose -f docker-compose.yml exec app python manage.py migrate --noinput
 # スーパーユーザー作成
-docker compose -f docker-compose.yml exec app python manage.py createsuperuser
+# docker compose -f docker-compose.yml exec app python manage.py createsuperuser
--- Django_App/life/views.py
+++ Django_App/life/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def loginfunc(request):
+    return render(request, 'login.html')
