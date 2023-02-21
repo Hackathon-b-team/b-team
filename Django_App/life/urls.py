1951f055ddb01734ffa82f2cd92139a6749a4ccb
hitugihane <hituzihane01@gmail.com>
2023-02-21 22:59:04 +0900

    詳細画面と編集画面、削除機能を実装

--- Django_App/life/urls.py
+++ Django_App/life/urls.py
@@ -9,6 +9,8 @@ from .views import (
     BarcodeView,
     BookAddView,
     DetailView,
+    DetailDeleteView,
+    DetailUpdateView,
     MoneyView,
     UserUpdateView
 )
@@ -23,9 +25,12 @@ urlpatterns = [
     path('logout/', CustumLogoutView.as_view(), name='logout'),
     path('home/', HomeView.as_view(), name='home'),
     path('detail/<uuid:uuid>/', DetailView.as_view(), name='detail'),
+    path('book_delete/<uuid:pk>/', DetailDeleteView.as_view(), name='book_delete'),
+    path('book_update/<uuid:pk>/',DetailUpdateView.as_view(), name='book_update'),
     path('barcode/', BarcodeView.as_view(), name='barcode'),
     path('add/', BookAddView.as_view(), name='add'),
     path('money/', MoneyView.as_view(), name='money'),
     path('mypage/', UserUpdateView.as_view(), name='mypage'),
-    path('social-auth/', include('social_django.urls', namespace='social')), # googlelogin
+    path('social-auth/', include('social_django.urls',
+         namespace='social')),  # googlelogin
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
