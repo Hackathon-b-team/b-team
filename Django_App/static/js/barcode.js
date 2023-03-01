851820797c92391ab2331a4015e8d4b45930acab
mappii130 <90373590+mappii130@users.noreply.github.com>
2023-03-02 00:04:08 +0900

    barcode画面完成

--- Django_App/static/js/barcode.js
+++ Django_App/static/js/barcode.js
@@ -8,10 +8,16 @@ const imageArea = document.getElementById("image-area");
 imageArea.appendChild(img);
 
 label.setAttribute("id","form-group-update-label");
-label.innerText = "ファイルを選択"
+label.innerText = "+"
 fileInput.before(label);
 label.appendChild(fileInput);
 
 fileInput.addEventListener('change', e => {
     img.src = window.URL.createObjectURL(e.target.files[0]);
+});
+
+// クリック時にlabel要素を消す
+const labelElement = document.querySelector('label');
+labelElement.addEventListener('click', function() {
+    labelElement.style.display = 'none';
 });
