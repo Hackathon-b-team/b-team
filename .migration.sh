956e97d592e675097a51f8315de7316b984c7a3c
daisuke imamura <d-imamura@purewest.dip.jp>
2023-03-03 12:02:36 +0900

    本番環境移植

--- .migration.sh
+++ .migration.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # マイグレーションを実行
-docker compose -f docker-compose.yml exec app python manage.py makemigrations --noinput
-docker compose -f docker-compose.yml exec app python manage.py migrate --noinput
+docker compose -f docker-compose.prod.yml exec app python manage.py makemigrations --noinput
+docker compose -f docker-compose.prod.yml exec app python manage.py migrate --noinput
+docker compose -f docker-compose.prod.yml exec app python manage.py makemigrations life --noinput
+docker compose -f docker-compose.prod.yml exec app python manage.py migrate life --noinput
+docker compose -f docker-compose.prod.yml exec app python manage.py sessions
 # スーパーユーザー作成
 # docker compose -f docker-compose.yml exec app python manage.py createsuperuser
